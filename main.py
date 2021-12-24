4ecdfe99a0f6f289d01634db16b08545245e016c
Marco <759374+mhelmich@users.noreply.github.com>
2021-12-23 20:59:28 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from typing import Optional
+import fastapi
+
+app = fastapi.FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
